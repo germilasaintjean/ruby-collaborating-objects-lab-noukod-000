fb21a996fbf559291b23c8ddae321695baec6f84
Saint-Jean Germila <germila@noukod.com>
2018-10-11 15:00:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ class Artist
   attr_accessor :name, :songs
 
 @@songs = []
+@@all = []
   def initialize(name)
     @name = name
     @songs = []
@@ -11,6 +12,7 @@ def add_song(song)
   @songs << song
   song.artist=self
   @@songs << song
+  @@all << song.artist
 end
 
 end
