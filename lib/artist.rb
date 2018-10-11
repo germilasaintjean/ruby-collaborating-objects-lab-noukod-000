ad5499db3247c99b5b097bbf37b7ec3bf86a6aa6
Saint-Jean Germila <germila@noukod.com>
2018-10-11 15:42:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,4 +20,27 @@ def self.all
   @@all
 end
 
+def self.find_or_create_by_name(name)   
+   if self.find(name)
+     self.find(name)
+   else
+     self.create(name)
+   end
+ end
+
+ def self.find(name)     
+   self.all.detect { |artist| artist.name == name }
+ end
+
+ def self.create(name)   
+   artist = Artist.new(name)
+   artist.save
+   artist
+ end
+
+ def print_songs
+  puts songs.map  {|song| song.name}.join("\n")
+end
+end
+
 end
